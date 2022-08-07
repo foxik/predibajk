0be55cd9767e99a5d39ffc2bd441684555d59ab6
Milan Straka <milan@strakovi.com>
2022-08-07 23:16:28 +0200

    Minor fixes for consistency.

--- download_images.sh
+++ download_images.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 read header
-echo -e "image_path\t$header\tcategory_short"
+echo -e "image_path\t$header"
 
 mkdir -p images
 
@@ -10,6 +10,7 @@ while read line; do
   path="images/$(printf %04d $i)"
   wget --quiet "$(echo "$line" | cut -f1)" -O $path || { echo Failed for image $i $line >&2; continue; }
   convert "$path" -fill white -opaque none -flatten -resize x480 -quality 97 "$path.jpg"
-  echo -e "$path.jpg\t$line\t$(echo "$line" | sed 's/^.*\t//;s/-e*bike$//')"
+  echo -e "$path.jpg\t$line"
+  rm "$path"
   i=$(($i + 1))
 done
